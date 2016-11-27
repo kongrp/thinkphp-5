e59084607df79a545609f0734b8b225ec74385c5
kongrp <960139253@qq.com>
2016-11-27 13:34:49 +0800

    3.7.3
    三种url结果一致，这是不对的

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -10,69 +10,192 @@ class TeacherController extends Controller
 {
     public function index()
     {
-        // $Teacher 首写字大写，说明它是一个对象, 更确切一些说明这是基于Teacher这个模型被我们手工实例化得到的，如果存在teacher数据表，它将对应teacher数据表。
-        $Teacher = new Teacher; 
+       try {
+            $pageSize = 5; // 每页显示5条数据
 
-        // $teachers 以s结尾，表示它是一个数组，数据中的每一项都是一个对象，这个对象基于Teahcer这个模型。
-        $teachers = $Teacher->select();
+            // 实例化Teacher
+            $Teacher = new Teacher; 
 
-        // 向V层传数据
-        $this->assign('teachers', $teachers);
-        // 取回打包后的数据
-        $htmls = $this->fetch();
-        // 将数据返回给用户
-        return $htmls;
+            // 调用分页
+            $teachers = $Teacher->limit(5)->select();
+
+            // 向V层传数据
+            $this->assign('teachers', $teachers);
+
+            // 取回打包后的数据
+            $htmls = $this->fetch();
+
+            // 将数据返回给用户
+            return $htmls;
+
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+            throw $e;
+
+        // 获取到正常的异常时，输出异常
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        } 
     }
 
     public function insert()
     {
-        // 接收传入数据
-        $postData = Request::instance()->post();    
+       $message = '';  // 提示信息
 
-        // 实例化Teacher空对象
-        $Teacher = new Teacher();
+        try {
+            // 接收传入数据
+            $postData = Request::instance()->post();    
 
-        // 为对象赋值
-        $Teacher->name = $postData['name'];
-        $Teacher->username = $postData['username'];
-        $Teacher->sex = $postData['sex'];
-        $Teacher->email = $postData['email'];
+            // 实例化Teacher空对象
+            $Teacher = new Teacher();
 
-        // 新增对象至数据表
-         $result = $Teacher->validate(true)->save();
+            // 为对象赋值
+            $Teacher->name = $postData['name'];
+            $Teacher->username = $postData['username'];
+            $Teacher->sex = $postData['sex'];
+            $Teacher->email = $postData['email'];
 
-        // 反馈结果
-        if(false === $result)
-        {
-            return '新增失败:' . $Teacher->getError();
-        } else {
-            return  '新增成功。新增ID为:' . $Teacher->id;
-        }
+            // 新增对象至数据表
+            $result = $Teacher->validate(true)->save();
+
+            // 反馈结果
+            if (false === $result)
+            {
+                // 验证未通过，发生错误
+                $message = '新增失败:' . $Teacher->getError();
+            } else {
+                // 提示操作成功，并跳转至教师管理列表
+                return $this->success('用户' . $Teacher->name . '新增成功。', url('index'));
+            }
+
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+            throw $e;
+
+        // 获取到正常的异常时，输出异常
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        } 
+
+        return $this->error($message);
     }
 
     public function add()
     {
-        $htmls = $this->fetch();
-        return $htmls;
+       try {
+            $htmls = $this->fetch();
+            return $htmls;
+        } catch (\Exception $e) {
+            return '系统错误' . $e->getMessage();
+        }
     }
 
     public function delete()
     {
-        // 获取get数据
-        var_dump(Request::instance()->get());
-        return;
+        // 获取pathinfo传入的ID值.
+        $id = Request::instance()->param('id/d'); // /d 表示将数值转化为 整形
 
-        // 获取要删除的对象
-        $Teacher = Teacher::get(14);
-
-        // 要删除的对象存在
-        if (!is_null($Teacher)) {
-            // 删除对象
-            if ($Teacher->delete()) {
-                return '删除成功';
-            }
+        if (is_null($id) || 0 === $id) {
+            return $this->error('未获取到ID信息');
         }
 
-        return '删除失败';
+        // 获取要删除的对象
+        $Teacher = Teacher::get($id);
+
+        // 要删除的对象不存在
+        if (is_null($Teacher)) {
+            return $this->error('不存在id为' . $id . '的教师，删除失败');
+        }
+
+        // 删除对象
+        if (!$Teacher->delete()) {
+            return $this->error('删除失败:' . $Teacher->getError());
+        }
+
+        // 进行跳转
+        return $this->success('删除成功', url('index'));
+    }
+
+    public function edit()
+    {
+        try {
+            // 获取传入ID
+            $id = Request::instance()->param('id/d');
+
+            // 在Teacher表模型中获取当前记录
+            if (null === $teacher = Teacher::get($id))
+            {
+                // 由于在$this->error抛出了异常，所以也可以省略return(不推荐)
+                $this->error('系统未找到ID为' . $id . '的记录');
+            } 
+
+            // 将数据传给V层
+            $this->assign('teacher', $teacher);
+
+            // 获取封装好的V层内容
+            $htmls = $this->fetch();
+
+            // 将封装好的V层内容返回给用户
+            return $htmls;
+
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+            throw $e;
+
+        // 获取到正常的异常时，输出异常
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        } 
+    }
+
+    public function update()
+    {
+        try {
+            // 接收数据，取要更新的关键字信息
+            $id = Request::instance()->post('id/d');
+
+            // 获取当前对象
+            $teacher = Teacher::get($id);
+
+            if (!is_null($teacher)) {
+                // 写入要更新的数据
+                $teacher->name = input('post.name');
+                $teacher->username = input('post.username');
+                $teacher->sex = input('post.sex');
+                $teacher->email = input('post.email');
+
+                // 更新
+                if (false === $teacher->validate(true)->save()) {
+                    return $this->error('更新失败' . $teacher->getError());
+                }
+            } else {
+                throw new \Exception("所更新的记录不存在", 1);   // 调用PHP内置类时，需要在前面加上 \ 
+            }
+
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+            throw $e;
+
+        // 获取到正常的异常时，输出异常
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        } 
+
+        // 成功跳转至index触发器
+        return $this->success('操作成功', url('index'));
+    }
+
+    public function test()
+    {
+            $pageSize = 5; // 每次显示5条数据
+            $Teacher = new Teacher; 
+
+            // 调用分页
+            $teachers = $Teacher->paginate($pageSize);
+            var_dump($teachers);
+
+            // 不调用分页
+            $teachers = $Teacher->select();
+            var_dump($teachers);
     }
 }
---
+++ public/test.php
@@ -0,0 +1,34 @@
+<?php
+header("Content-Type:text/html;charset=utf-8");
+class Test
+{
+    public function sayHello()
+    {  
+        echo "hello <br />";
+
+    }
+
+    public function sayOther($words)
+    {
+        echo "$words <br />";
+    }
+
+    public function functionNotExist()
+    {
+        echo '您要调用的方法不存在 <br />';
+    }
+
+    public function __call($method, $args)
+    {
+        echo "你要调用的方法是:$method <br />";
+        echo "传入参数是:";
+        var_dump($args);
+        $this->functionNotExist($args);
+    }
+}
+$Test = new Test();
+// 在这下面，我们开始写测试代码。
+// $Test->sayHello();
+// $Test->sayOther('hello yunzhi');
+//$Test->ssd('hi Yunzhier, how a u');
+$Test->myz('hi Yunzhier', 'how a u');
